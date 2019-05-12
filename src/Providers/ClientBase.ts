97161d7e1a9e371ff4631870687bb2f3aa14f43b
gavanlamb <gavan.jon.lamb@gmail.com>
2019-05-12 13:32:38 +1000

    use async await

--- src/Providers/ClientBase.ts
+++ src/Providers/ClientBase.ts
@@ -22,66 +22,48 @@ export class ClientBase implements IClientBase
     public async processResponse<T>(
         res: HttpClientResponse,
         options: RestClientIRequestOptions): Promise<IRestResponse<T>> {
-        return new Promise<IRestResponse<T>>(async (resolve, reject) => {
-            const statusCode: number = res.message.statusCode;
+        const statusCode: number = res.message.statusCode;
 
-            const response: IRestResponse<T> = {
-                statusCode: statusCode,
-                result: null,
-            };
+        const response: IRestResponse<T> = {
+            statusCode: statusCode,
+            result: null,
+        };
 
-            // not found leads to null obj returned
-            if (statusCode == HttpCodes.NotFound) {
-                resolve(response);
+        // not found leads to null obj returned
+        if (statusCode == HttpCodes.NotFound) return response;
+
+        let obj: any;
+
+        // get the result from the body
+        try {
+            let contents: string = await res.readBody();
+            if (contents && contents.length > 0) {
+                if (options && options.deserializeDates) obj = JSON.parse(contents, ClientBase.dateTimeDeserializer);
+                else obj = JSON.parse(contents);
+                if (options && options.responseProcessor) response.result = options.responseProcessor(obj);
+                else response.result = obj;
             }
+        }
+        catch (err) {
+            // Invalid resource (contents not json);  leaving result obj null
+        }
 
-            let obj: any;
+        // note that 3xx redirects are handled by the http layer.
+        if (statusCode > 299) {
+            let msg: string;
 
-            // get the result from the body
-            try {
-                let contents: string = await res.readBody();
-                if (contents && contents.length > 0) {
-                    if (options && options.deserializeDates) {
-                        obj = JSON.parse(contents, this.dateTimeDeserializer);
-                    } else {
-                        obj = JSON.parse(contents);
-                    }
-                    if (options && options.responseProcessor) {
-                        response.result = options.responseProcessor(obj);
-                    }
-                    else {
-                        response.result = obj;
-                    }
-                }
-            }
-            catch (err) {
-                // Invalid resource (contents not json);  leaving result obj null
-            }
+            // if exception/error in body, attempt to get better error
+            if (obj && obj.message) msg = obj.message;
+            else msg = "Failed request: (" + statusCode + ")";
 
-            // note that 3xx redirects are handled by the http layer.
-            if (statusCode > 299) {
-                let msg: string;
+            let err: Error = new Error(msg);
 
-                // if exception/error in body, attempt to get better error
-                if (obj && obj.message) {
-                    msg = obj.message;
-                } else {
-                    msg = "Failed request: (" + statusCode + ")";
-                }
+            // attach statusCode and body obj (if available) to the error object
+            err['statusCode'] = statusCode;
+            if (response.result) err['result'] = response.result;
 
-                let err: Error = new Error(msg);
-
-                // attach statusCode and body obj (if available) to the error object
-                err['statusCode'] = statusCode;
-                if (response.result) {
-                    err['result'] = response.result;
-                }
-
-                reject(err);
-            } else {
-                resolve(response);
-            }
-        });
+            throw err;
+        } else return response;
     }
 
     static createRequestHeaders(
